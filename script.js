2e60efd6dee909aed288a9292dbd6b90ecc621d2
Shefakopling <shefagilang@gmail.com>
2026-06-04 23:00:18 +0700

    Add event listeners for button interactions

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-sfd
+const btnJawab = document.getElementById("btnJawab");
+const jawaban = document.getElementById("jawaban");
+
+btnJawab.addEventListener("click", () => {
+
+    if(jawaban.classList.contains("hidden")){
+        jawaban.classList.remove("hidden");
+        btnJawab.innerHTML = "Sembunyikan Jawaban";
+    }else{
+        jawaban.classList.add("hidden");
+        btnJawab.innerHTML = "Tampilkan Jawaban";
+    }
+
+});
+
+const btnMateri = document.getElementById("btnMateri");
+
+btnMateri.addEventListener("click", () => {
+
+    document
+        .getElementById("materi")
+        .scrollIntoView({
+            behavior:"smooth"
+        });
+
+});
